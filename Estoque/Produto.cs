d4edbce6409e76370a161ff3dfbe5cdb1547545d
Israilla <israillacaroline@hotmail.com>
2020-03-22 19:44:39 -0300

    Iniciando o programa

---
+++ Estoque/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+            }
+
+                public override string ToString()
+        {
+            return Nome + ", $ " + Preco;
+    }
+        }
+    }
+
